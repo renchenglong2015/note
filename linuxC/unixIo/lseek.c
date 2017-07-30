da7701461e9d94e8c50240677d6c463b265cf9ea
rcl <523465560@qq.com>
2017-07-30 16:31:57 +0800

    修改lseek

--- linuxC/unixIo/lseek.c
+++ linuxC/unixIo/lseek.c
@@ -1,2 +1,27 @@
-#inlcude <stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd = open("aaa", O_RDWR);
+    if(fd == -1){
+        perror("open file");
+        exit(1);
+    }  
+    int ret = lseek(fd, 0, SEEK_END);
+    printf("file len = %d\n", ret);
+
+    //文件拓展
+    ret = lseek(fd, 2000, SEEK_END);
+    printf("return val %d\n", ret);
+    //实现文件拓展，需要最后做一次写操作
+    write(fd, "a", 1);
+    close(fd);
+    return 0;
+
+}
 
