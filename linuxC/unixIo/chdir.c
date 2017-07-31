b73b0a6bdee2021cbe9650e821f631dacf14be9d
rcl <523465560@qq.com>
2017-07-31 12:02:17 +0800

    chdir getcwd

---
+++ linuxC/unixIo/chdir.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc,char* argv[])
+{
+    if(argc < 2)
+    {
+        printf("a.out dir\n");
+        exit(1);
+    }
+    //修改当前进程的路径
+    int ret = chdir(argv[1]);
+
+    if(ret == -1)
+    {
+        perror("chdir");
+        exit(1);
+    }
+
+    int fd = open("renchenglong.txt", O_CREAT | O_RDWR, 0777);
+    if(fd == -1)
+    {
+        perror("open");
+        exit(1);
+    }
+    close(fd);
+
+    char buf[64];
+    //获取当前进程工作目录
+    getcwd(buf, sizeof(buf));
+
+    printf("当前工作目录是: %s\n", buf);
+
+    return 0;
+
+    
+}
