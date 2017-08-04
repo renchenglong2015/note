407b3995ab51aefece44fc0761cdbdd7bf1f89f7
rcl <523465560@qq.com>
2017-08-04 18:41:16 +0800

    生成指定个进程

--- linuxC/multProcess/fork.c
+++ linuxC/multProcess/fork.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[])
+{
+    //默认创建5个进程
+    int n = 5, i;
+    
+    if(argc == 2){
+        n = atoi(argv[1]);
+    }
+    for(i = 0; i < n; i++){
+        if(fork() == 0){
+            break;
+        }
+    }
+    if(n == i){
+        sleep(n);
+        printf("i am parent, pid =%d\n", getpid());
+    }else{
+        sleep(i);
+        printf("i am %dth child, pid = %d\n", i+1, getpid());
+    }
+    return 0;
+}
 
