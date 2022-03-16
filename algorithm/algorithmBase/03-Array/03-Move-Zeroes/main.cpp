bf6398c4fe7dcc3e4e84b2f06209b9cee3b968ee
renchenglong <523465560@qq.com>
2022-03-16 11:05:21 +0800

    移动零

--- algorithm/algorithmBase/03-Array/03-Move-Zeroes/main.cpp
+++ algorithm/algorithmBase/03-Array/03-Move-Zeroes/main.cpp
@@ -2,3 +2,40 @@
 #include <vector>
 
 using namespace std;
+
+/**
+ * @brief移动零
+ *
+ */
+class Solution
+{
+public:
+    void moveZeros(vector<int> &nums)
+    {
+        //[0,k) k代表为前n个非零元素
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i])
+            {
+                if (i != k)
+                {
+                    swap(nums[i], nums[k]);
+                    k++;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    int arr[] = {0, 1, 0, 3, 12};
+    vector<int> vec(arr, arr + sizeof(arr) / sizeof(int));
+
+    Solution().moveZeros(vec);
+
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
